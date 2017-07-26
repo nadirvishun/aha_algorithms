677a93774b2e3a083217b2b72128676201bc4dab
nadirvishun <nadirvishun@gmail.com>
2017-07-26 22:30:57 +0800

    修改优化冒泡排序

--- 1_sort/bubble_sort.c
+++ 1_sort/bubble_sort.c
@@ -10,7 +10,7 @@ struct student
 int main()
 {
 	struct student a[100],t;
-	int i,j,n;
+	int i,j,n,flag;
 	printf("please input total number:");
 	//输入个数
 	scanf("%d",&n);
@@ -21,10 +21,15 @@ int main()
 	}
 	//排序(从高到低)
 	for(i=1;i<=n-1;i++){
+		flag=1;//优化
 		for(j=1;j<=n-i;j++){
 			if(a[j].score<a[j+1].score){
 				t=a[j];a[j]=a[j+1];a[j+1]=t;
+				flag=0;
 			}
+		}
+		if(flag==1){
+			break;
 		}
 	}
 	//输出名字及分数
